e005c78cd4edbaa35a3b2b18bdb9428f771c32b4
Spruce Campbell <spruce@mit.edu>
2024-10-31 17:55:25 -0400

    fixed links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
 
         <a
             className="rounded-full border border-solid border-foreground transition-colors flex items-center justify-center gap-2 hover:bg-[#ccc] dark:hover:bg-[#383838] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="http://www.decart.ai/articles/oasis-interactive-ai-video-game-model"
+            href="https://www.decart.ai/articles/oasis-interactive-ai-video-game-model"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -242,7 +242,7 @@ export default function Home() {
         <Image src="/dyno.png" alt="Dynamic Noising" width={700} height={500} />
         </div>
 
-        <p className="text-justify leading-relaxed">To learn more about the engineering underlying this model, and some of the specific optimizations in training and inference, check out the <a className="underline" href="https://decart.ai/blog/training-the-world-model">Decart blog post</a>.</p>
+        <p className="text-justify leading-relaxed">To learn more about the engineering underlying this model, and some of the specific optimizations in training and inference, check out the <a className="underline" href="https://www.decart.ai/articles/oasis-interactive-ai-video-game-model">Decart blog post</a>.</p>
 
         <div className="relative w-screen left-1/2 right-1/2 -mx-[50vw] my-4">
           <Carousel className="w-full" opts={{ loop: true }}>
@@ -278,7 +278,7 @@ export default function Home() {
         <Image src="/speed.png" alt="Performance" width={500} height={500} />
         </section>
 
-        <p className="text-justify leading-relaxed">With Decart&apos;s inference stack, the model runs at playable framerates, unlocking real-time interactivity for the first time. Read more about it on <a className="underline" href="https://decart.ai/blog/training-the-world-model">Decart&apos;s blog</a>.</p>
+        <p className="text-justify leading-relaxed">With Decart&apos;s inference stack, the model runs at playable framerates, unlocking real-time interactivity for the first time. Read more about it on <a className="underline" href="https://www.decart.ai/articles/oasis-interactive-ai-video-game-model">Decart&apos;s blog</a>.</p>
 
         <p className="text-justify leading-relaxed">However, to make the model an additional order of magnitude faster, and make it cost-efficient to run at scale, new hardware is needed. Oasis is optimized for Sohu, the Transformer ASIC built by Etched. Sohu can scale to massive 100B+ next-generation models in 4K resolution.</p>
 
@@ -359,7 +359,7 @@ export default function Home() {
       <div className="flex flex-col gap-4 px-0 md:px-16 bg-black">
         <div className="flex flex-col gap-4 p-8 pb-20 gap-16 sm:p-20 md:px-48">
               <h2 className="text-2xl font-bold text-center text-white">Contributors</h2>
-              <p className="text-white text-center"><a className="underline" href="http://decart.ai/">Decart Team</a></p>
+              <p className="text-white text-center"><a className="underline" href="http://decart.ai/">AI Team at Decart</a></p>
               <p className="text-white text-center"><a className="underline" href="https://www.etched.com/">Etched</a>: <a className="underline" href="https://julian-q.github.io/">Julian Quevedo</a>, <a className="underline" href="https://www.linkedin.com/in/quinn-mcintyre/">Quinn McIntyre</a>, <a className="underline" href="https://www.linkedin.com/in/spruce-campbell-5a13061a6/">Spruce Campbell</a>, <a className="underline" href="https://xinleic.xyz/">Xinlei Chen</a>, <a className="underline" href="https://www.linkedin.com/in/robertwachen/">Robert Wachen</a></p>
               
         </div>
