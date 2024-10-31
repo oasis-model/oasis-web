94e1e51d8c4c5b1db626afcb98f22c42fb3c5dd0
Spruce Campbell <spruce@mit.edu>
2024-10-31 16:28:06 -0400

    ixed apos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -283,7 +283,7 @@ export default function Home() {
 
         <p className="text-justify leading-relaxed">However, to make the model an additional order of magnitude faster, and make it cost-efficient to run at scale, new hardware is needed. Oasis is optimized for Sohu, the Transformer ASIC built by Etched. Sohu can scale to massive 100B+ next-generation models in 4K resolution.</p>
 
-        <p className="text-justify leading-relaxed">In addition, Oasis' end-to-end Transformer architecture makes it extremely efficient on Sohu, which can serve &gt;10x more users even on 100B+ parameter models. We believe the price of serving models like Oasis is the hidden bottleneck to releasing generative video in production. See more performance figures and read more about Oasis and Sohu on <a className="underline" href="https://etched.ai/blog">Etched&apos;s blog</a>.</p>
+        <p className="text-justify leading-relaxed">In addition, Oasis&apos; end-to-end Transformer architecture makes it extremely efficient on Sohu, which can serve &gt;10x more users even on 100B+ parameter models. We believe the price of serving models like Oasis is the hidden bottleneck to releasing generative video in production. See more performance figures and read more about Oasis and Sohu on <a className="underline" href="https://etched.ai/blog">Etched&apos;s blog</a>.</p>
 
         <section className="flex justify-center items-center my-4">
         <Image src="/users.png" alt="Performance" width={500} height={500} />
