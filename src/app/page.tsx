bae6c1c59c91874ee17b166391672a252e75c3df
Spruce Campbell <spruce@mit.edu>
2024-11-01 14:13:08 -0700

    xinlei

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -361,8 +361,9 @@ export default function Home() {
         <div className="flex flex-col gap-4 p-8 pb-20 gap-16 sm:p-20 md:px-48">
               <h2 className="text-2xl font-bold text-center text-white">Contributors</h2>
               <p className="text-white text-center"><a className="underline" href="http://decart.ai/">AI Team at Decart</a></p>
-              <p className="text-white text-center"><a className="underline" href="https://www.etched.com/">Etched</a>: <a className="underline" href="https://julian-q.github.io/">Julian Quevedo</a>, <a className="underline" href="https://www.linkedin.com/in/quinn-mcintyre/">Quinn McIntyre</a>, <a className="underline" href="https://www.linkedin.com/in/spruce-campbell-5a13061a6/">Spruce Campbell</a>, <a className="underline" href="https://xinleic.xyz/">Xinlei Chen</a>, <a className="underline" href="https://www.linkedin.com/in/robertwachen/">Robert Wachen</a></p>
-              
+              <p className="text-white text-center"><a className="underline" href="https://www.etched.com/">Etched</a>: <a className="underline" href="https://julian-q.github.io/">Julian Quevedo</a>, <a className="underline" href="https://www.linkedin.com/in/quinn-mcintyre/">Quinn McIntyre</a>, <a className="underline" href="https://www.linkedin.com/in/spruce-campbell-5a13061a6/">Spruce Campbell</a>, <a className="underline" href="https://www.linkedin.com/in/robertwachen/">Robert Wachen</a></p>
+              <p className="text-white text-center">Thanks to: <a className="underline" href="https://xinleic.xyz/">Xinlei Chen</a></p>
+    
         </div>
         </div>
 
